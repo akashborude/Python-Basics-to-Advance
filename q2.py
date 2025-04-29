7f6b71f0b842be5316d892c1e2ac25ebc1584768
akashborude <akashborude@21gmail.com>
2025-04-29 13:43:35 +0530

    new changes

--- q2.py
+++ q2.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Write a program to fill in letter template given below with name and date
 
 name = "Akash"
@@ -11,11 +11,4 @@ let = '''Dear <|Name|>
             <|Date|>'''
 
 print(let.replace("<|Name|>","Akash").replace("<|Date|>", "29-04-2025"))
-=======
-# Write a program to find remainder when a number is divided by z
 
-a = 4
-b = 2
-c = a%b
-print(c)
->>>>>>> origin/master
--- q3.py
+++ q3.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 # Write a program to find double space in a String 
 
 str = "aka  sh  "
 new_str = str.find("  ")
 
 print(new_str)
-=======
-# check the type of variable assigned using input function 
 
-a = input(23)
-print(type())
->>>>>>> origin/master
+
